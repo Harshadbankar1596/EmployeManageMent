0324186d76b2c199bd28f38683f4b426485bd60a
Harshadbankar1596 <harshadbankar1596@gmail.com>
2025-08-07 10:06:04 +0530

    true kardiya login ko ... http ko hataya sameSite none

--- backend/controller/usercontroller.js
+++ backend/controller/usercontroller.js
@@ -56,7 +56,7 @@ export const loginUser = async (req, res) => {
 
         if (remember) {
             token = jwt.sign({ id: user._id }, process.env.ACCESS_TOKEN_SECRET || "secretkey", { expiresIn: '7d' });
-            res.cookie("token", token, { httpOnly: true, secure: true, Samesite : 'None' , maxAge: 7 * 24 * 60 * 60 * 1000 });
+            res.cookie("token", token, { httpOnly: true, secure: true, Samesite: 'None'  , maxAge: 7 * 24 * 60 * 60 * 1000 });
         }
 
         else {
