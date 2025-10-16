5c0ecb80500d7d9ff832616ac7f116aac1d2be23
Harshadbankar1596 <harshadbankar1596@gmail.com>
2025-10-16 15:34:46 +0530

    kasm

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.VITE_APP_URL || "*",
+    origin: "*",
     credentials: true,               
 }));
 
